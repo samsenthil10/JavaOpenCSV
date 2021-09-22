5b71c9eb8484d394fec78a0487f9e76db93d160b
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-22 05:39:16 +0530

    Added: To read data from csv and write to json file

---
+++ src/main/java/com/bridgelabz/javaopencsv/OpenCSVAndGSONTester.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.javaopencsv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class OpenCSVAndGSONTester {
+
+	private static final String SAMPLE_CSV_FILE_PATH = "./users-with-header.csv";
+	private static final String SAMPLE_JSON_FILE_PATH = "./users.json";
+
+	public static void main(String[] args) {
+
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+
+			CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder<CSVUser>(reader)
+					.withType(CSVUser.class)
+					.withIgnoreLeadingWhiteSpace(true)
+					.withSeparator('|')
+					.build();
+			List<CSVUser> csvUsers = csvToBean.parse();
+			Gson gson = new Gson();
+			String json = gson.toJson(csvUsers);
+			FileWriter writer = new FileWriter(SAMPLE_JSON_FILE_PATH);
+			writer.write(json);
+			writer.close();
+
+			BufferedReader br = new BufferedReader(new FileReader(SAMPLE_JSON_FILE_PATH));
+			CSVUser[] usrObj = gson.fromJson(br, CSVUser[].class);
+			List<CSVUser> csvUserList = Arrays.asList(usrObj);
+			csvUserList.stream().forEach(hotel->{System.out.println(hotel);});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+} 
